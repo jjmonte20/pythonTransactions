e5eec76df2f67c6f3769ebaa1effb783ae809c13
jjmonte20 <jjmonte20@gmail.com>
2019-06-03 15:09:50 -0500

    commented chart data, it is to be used when a chart needs to be made

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import openpyxl as xl
-from openpyxl.chart import BarChart, Reference
+# from openpyxl.chart import BarChart, Reference
+
 
 def process_workbook(filename):
     wb = xl.load_workbook(filename)
@@ -11,14 +12,10 @@ def process_workbook(filename):
         corrected_price_cell = sheet.cell(row, 4)
         corrected_price_cell.value = corrected_price
 
-    values = Reference(sheet,
-              min_row=2,
-              max_row=sheet.max_row,
-              min_col=4,
-              max_col=4)
+    # values = Reference(sheet, min_row=2, max_row=sheet.max_row, min_col=4, max_col=4)
 
-    chart = BarChart()
-    chart.add_data(values)
-    sheet.add_chart(chart, 'E2')
+    # chart = BarChart()
+    # chart.add_data(values)
+    # sheet.add_chart(chart, 'E2')
 
     wb.save(filename)
